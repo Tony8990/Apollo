abe90043aa66d67b1257d8f8dbb2508eaf024f45
Tony Fava <tony.fava@deltatre.it>
2019-12-20 09:30:06 +0100

    Apollo-server

--- index.js
+++ index.js
@@ -9,13 +9,9 @@ const typeDefs = gql `
   # This "Book" type defines the queryable fields for every book in our data source.
   type Book {
     title: String
-    author: Author
+    author: String
   }
 
-  type Author {
-    name: String
-    books: [Book]
-  }
   # The "Query" type is special: it lists all of the available queries that
   # clients can execute, along with the return type for each. In this
   # case, the "books" query returns an array of zero or more Books (defined above).
@@ -31,28 +27,21 @@ const books = [{
         title: 'Jurassic Park',
         author: 'Michael Crichton',
     },
-    {
-        title: 'Fimminaru alla Riscossa',
-        author: 'Davide Fassola'
-    },
-    {
-        title: 'Io sono Sciacallo',
-        author: 'Albaneeeeeeseeeeee'
-    }
 ];
+
 // Resolvers define the technique for fetching the types defined in the
 // schema. This resolver retrieves books from the "books" array above.
 const resolvers = {
     Query: {
         books: () => books,
-    }
-
+    },
 };
+
 // The ApolloServer constructor requires two parameters: your schema
 // definition and your set of resolvers.
 const server = new ApolloServer({ typeDefs, resolvers });
 
 // The `listen` method launches a web server.
 server.listen({ port: process.env.PORT || 4000 }).then(({ url }) => {
-    console.log(`🚀  Server ready at ${url}`);
+    console.log(`🚀 Server ready at ${url}`);
 });
